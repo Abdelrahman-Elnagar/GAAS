63393e542012ead3c9832a04c68a4c1bf1eeeee9
coolAbdo <abdelrahman.egschool@gmail.com>
2025-05-14 17:16:43 +0300

    add access control for authenticated users in storage configuration

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -2,4 +2,10 @@ import { defineStorage } from '@aws-amplify/backend';
 
 export const storage  = defineStorage({
   name: 'GaasStorage',
+  access: (allow) => ({
+    'files/{entity_id}/*': [
+      allow.authenticated.to(['read', 'write', 'delete']),
+    ],
+    isDefault: true
+  })
 });
