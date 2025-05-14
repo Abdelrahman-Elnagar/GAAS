11be39b96d565a30c1031cf547492915865785fd
coolAbdo <abdelrahman.egschool@gmail.com>
2025-05-14 19:12:21 +0300

    feat: Added negms part to the new app and fixed a storage declaration

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -3,9 +3,8 @@ import { defineStorage } from '@aws-amplify/backend';
 export const storage  = defineStorage({
   name: 'GaasStorage',
   access: (allow) => ({
-    'files/{entity_id}/*': [
+    'files/*': [
       allow.authenticated.to(['read', 'write', 'delete']),
     ],
-    isDefault: true
   })
 });
