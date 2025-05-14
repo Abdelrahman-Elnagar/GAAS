43bed87c8af5cf13acf8f97698218d1d564b77cb
coolAbdo <abdelrahman.egschool@gmail.com>
2025-05-14 18:23:12 +0300

    fix: i have no idea

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -2,9 +2,8 @@ import { defineStorage } from '@aws-amplify/backend';
 
 export const storage = defineStorage({
   name: 'GaasStorage',
-  isDefault: true,
   access: (allow) => ({
-    'files/{entity_id}/*': [
+    'files/*': [
       allow.authenticated.to(['read', 'write', 'delete']),
     ]
   })
